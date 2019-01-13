785533963d58a46cf4e6f6f1ff42a97ab6a86598
Daniel Burke <danieljburke@me.com>
2019-01-13 14:58:29 +1100

    now some sweet updates to the browser!

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const port = process.env.PORT || 3000;
 
 const { exec } = require('child_process');
 
+const expresssse = require('express-sse');
+
 app.get('/', (req, res) => {
 	res.sendFile( __dirname + '/index.html');
 });
 
 var vlc = null;
+var vlcStatus = new expresssse();
 app.post('/ts/vlc', (req, res) => {
 	if (vlc)
 	{
@@ -18,36 +21,29 @@ app.post('/ts/vlc', (req, res) => {
 		res.end();
 	}
 
-	console.log('yo');
+	vlc = exec('raspivid -o - -t 0 -hf -g 12 -w 800 -h 400 -fps 24 | cvlc -vvv stream:///dev/stdin --sout \'#standard{access=udp,mux=ts,dst=192.168.1.9:8160}\' :demux=h264');
+	
+	vlc.stdout.on('data', (data) => {
+		// console.log(`stdout: ${data}`);
+		vlcStatus.send(`VLC standard out: ${data}`);
+	});
 
-	try {
-		vlc = exec('raspivid -o - -t 0 -hf -g 12 -w 800 -h 400 -fps 24 | cvlc -vvv stream:///dev/stdin --sout \'#standard{access=udp,mux=ts,dst=192.168.1.9:8160}\' :demux=h264');
-		// vlc = exec('raspivid -o - -t 0 -hf -g 12 -w 800 -h 400 -fps 24 | cvlc');
+	vlc.stderr.on('data', (data) => {
+	  	// console.log(`stderr: ${data}`);
+	  	vlcStatus.send(`VLC standard error: ${data}`);
+	});
 
-		vlc.stdout.on('data', (data) => {
-		  console.log(`stdout: ${data}`);
-		});
+	vlc.on('close', (code) => {
+	  	vlcStatus.send(`VLC close: ${code}`);
+	  	// console.log(`child process exited with code ${code}`);
+	});
 
-		vlc.stderr.on('data', (data) => {
-		  console.log(`stderr: ${data}`);
-		});
+	vlc.on('exit', (code) => {
+		// console.log(`boom: ${code}`);
+		vlcStatus.send(`VLC exit: ${code}`);
+	});
 
-		vlc.on('close', (code) => {
-		  console.log(`child process exited with code ${code}`);
-		});
-
-		vlc.on('exit', (code) => {
-			console.log(`boom: ${code}`);
-		});
-	}
-	catch (error)
-	{
-		console.log(`error: ${error}`);
-	}
-	finally
-	{
-		res.end();		
-	}
+	res.end();
 });
 
 app.delete('/ts/vlc', (req, res) => {
@@ -55,5 +51,6 @@ app.delete('/ts/vlc', (req, res) => {
 	res.end();
 })
 
-//console.log(app._router.stack);
+app.get('/ts/vlc/status', vlcStatus.init);
+
 app.listen(port, () => console.log('Listening on port ' + port));
